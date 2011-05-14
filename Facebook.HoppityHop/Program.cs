28c2b5b9200a82dfae508b9f753f2c154dd0180e
unknown <Mrouane Atig@.(none)>
2011-05-15 01:28:40 +0200

    First commit

---
+++ Facebook.HoppityHop/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mérouane Atig
+ * Date: 15/05/2011
+ * Time: 01:26
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Facebook.HoppityHop
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length == 0) {
+				Console.WriteLine("You must provide a file name");
+			}
+			var filename = args[0];
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
