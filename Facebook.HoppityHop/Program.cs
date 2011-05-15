a0e4a7fee98047138ee15a25dd17ae60faaa40f9
Merouane Atig <merouane.atig@orkeis.com>
2011-05-15 16:04:56 +0200

    Algorithm impplementation

--- Facebook.HoppityHop/Program.cs
+++ Facebook.HoppityHop/Program.cs
@@ -1,39 +1,52 @@
-﻿/*
- * Created by SharpDevelop.
- * User: Mérouane Atig
- * Date: 15/05/2011
- * Time: 01:26
- * 
- * To change this template use Tools | Options | Coding | Edit Standard Headers.
- */
-using System;
+﻿using System;
 using System.IO;
 
 namespace Facebook.HoppityHop
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-			if (args.Length == 0) {
-				Console.WriteLine("You must provide a file name");
-			}
-			var filename = args[0];
-			
-			var content = File.ReadAllLines(filename);
-			
-			if (content.Length == 0) {
-				Console.WriteLine("The input file must contain an integer");
-			}
-			
-			var max = Convert.ToInt32(content[0]);
-			
-			for (int i = 0; i < max; i++) {
-				
-			}
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
-		}
-	}
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("You must provide a file name");
+                Exit();
+                return;
+            }
+            var filename = args[0];
+
+            var content = File.ReadAllLines(filename);
+
+            if (content.Length == 0)
+            {
+                Console.WriteLine("The input file must contain an integer");
+            }
+
+            var max = Convert.ToInt32(content[0]);
+
+            for (var i = 1; i <= max; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("Hop");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Hoppity");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Hophop");
+                }
+            }
+
+            Exit();
+        }
+
+        private static void Exit()
+        {
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
 }
